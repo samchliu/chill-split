dddec850f5ee9fc6cb73a60cb26e6b9911247a29
samchl <happysam31640@gmail.com>
2024-07-31 01:43:21 +0800

    Add icons
    Add camera, create group, delete, share icons

--- src/components/ui/icons/index.tsx
+++ src/components/ui/icons/index.tsx
@@ -3,6 +3,10 @@ import { DollarSignIcon } from "./dollar-sign-icon";
 import { StarIcon } from "./star-icon";
 import { MealIcon } from "./meal-icon";
 import { HeartIcon } from "./heart-icon";
+import { CreateGroupIcon } from "./create-group-icon";
+import { ShareIcon } from "./share-icon";
+import { CameraIcon } from "./camera-icon";
+import { DeleteIcon } from "./delete-icon";
 
 export {
   PlainIcon,
@@ -10,4 +14,8 @@ export {
   StarIcon,
   MealIcon,
   HeartIcon,
+  CreateGroupIcon,
+  ShareIcon,
+  CameraIcon,
+  DeleteIcon,
 };
