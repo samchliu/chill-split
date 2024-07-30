bb05dfe453cdd74a64d101ceadcac04b659fb3c8
samchl <happysam31640@gmail.com>
2024-07-31 01:45:28 +0800

    Modify next.config
    Add image remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "profile.line-scdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "fastly.picsum.photos",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
