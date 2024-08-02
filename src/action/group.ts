a753b945d1fe17ec77ed108339dbd6c172388fa9
samchl <happysam31640@gmail.com>
2024-08-02 20:35:44 +0800

    Add getGroupById action

--- src/action/group.ts
+++ src/action/group.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import prisma from "@/lib/db";
-import { redirect } from "next/navigation";
+import { redirect, notFound } from "next/navigation";
 
 type GroupData = {
   name: string;
@@ -28,4 +28,60 @@ export async function createGroup(groupData: GroupData) {
   });
 
   redirect(`/group/${group.id}`);
+}
+
+export async function getGroupById(groupId: string) {
+  const group = await prisma.group.findUnique({
+    where: { id: groupId },
+    include: {
+      groupUsers: {
+        select: {
+          user: true,
+        },
+      },
+      expenses: {
+        select: {
+          id: true,
+          name: true,
+          category: true,
+          amount: true,
+          date: true,
+          note: true,
+          payerId: true,
+          sharers: {
+            select: {
+              amount: true,
+              user: {
+                select: {
+                  id: true,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+  if (!group) notFound();
+
+  let sorted = {
+    id: group.id,
+    name: group.name,
+    picture: group.picture,
+    creatorId: group.creatorId,
+    users: group.groupUsers.map((item) => ({
+      id: item.user.id,
+      name: item.user.name,
+      picture: item.user.picture,
+      adoptable: !Boolean(item.user.lineId),
+    })),
+    expenses: group.expenses.map((expense) => ({
+      ...expense,
+      sharers: expense.sharers.map((sharer) => ({
+        id: sharer.user.id,
+        amount: sharer.amount,
+      })),
+    })),
+  };
+  return sorted;
 }
