9bb57143249890145f8f71f40a9c4e7846cad543
samchl <happysam31640@gmail.com>
2024-08-02 20:34:45 +0800

    Modify button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 
 type ButtonProps = React.ComponentPropsWithRef<"button"> & {
   size?: "lg" | "sm";
-  variant?: "destructive" | "secondary";
+  variant?: "destructive" | "secondary" | "neutral";
   pending?: string;
 };
 
@@ -19,9 +19,11 @@ export function Button({
       className={cn(
         "flex-center h-9 gap-x-2 rounded-full bg-brand-green text-foreground",
         size === "lg" && "h-[54px]",
-        size === "sm" && "h-9",
+        size === "sm" && "h-9 text-sm",
         variant === "destructive" && "bg-brand-pink",
         variant === "secondary" && "bg-brand-yellow",
+        variant === "neutral" && "bg-brand-neutral-20",
+        props.className,
       )}
     >
       {pending ? (
