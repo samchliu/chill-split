6af5e735284a61b6ab1e0d1c03ff94f09cd41818
samchl <happysam31640@gmail.com>
2024-08-13 22:21:47 +0800

    Add jwt utils

---
+++ src/lib/jwt.ts
@@ -0,0 +1,18 @@
+import { SignJWT, jwtVerify } from 'jose';
+
+const secretKey = process.env.ACCESS_TOKEN_SECRET;
+const key = new TextEncoder().encode(secretKey);
+const alg = 'HS256';
+
+export async function encrypt(payload: any) {
+  return await new SignJWT(payload)
+    .setProtectedHeader({ alg })
+    .setIssuedAt()
+    .setExpirationTime('15 min from now')
+    .sign(key);
+}
+
+export async function decrypt(input: string): Promise<any> {
+  const { payload } = await jwtVerify(input, key, { algorithms: [alg] });
+  return payload;
+}
