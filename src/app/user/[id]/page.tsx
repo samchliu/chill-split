be753af800f853d1a9721e8a6f120dc58e275ced
samchl <happysam31640@gmail.com>
2024-10-09 00:27:20 +0800

    Modify user page

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,7 +1,22 @@
-interface UserPageProps {}
+import { fetchUser } from './action';
+import CreateGroupButton from './_components/create-group-button';
 
-export default function UserPage(props: Readonly<UserPageProps>) {
+type UserPageProps = { params: { id: string } };
+
+export default async function UserPage(props: Readonly<UserPageProps>) {
+  const user = await fetchUser(props.params.id);
+  console.log(user);
+
   return (
-    <span>UserPage</span>
+    <main className='h-screen bg-primary px-6 text-primary-foreground'>
+      <h1 className='py-7 text-center text-2xl font-semibold'>群組列表</h1>
+      <CreateGroupButton user={user}/>
+      {/* <CreateGroupButton user={user}/> */}
+      <ul className='flex flex-col gap-4'>
+        {/* {user.groups.map((group) => (
+        <GroupCard key={group.id} {...group} />
+      ))} */}
+      </ul>
+    </main>
   );
 }
